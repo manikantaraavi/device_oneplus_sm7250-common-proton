3e8f9ee5dd244e956f764940606deab445e5f251
Manikantaraavi <raavimanikanta6595@gmail.com>
2022-02-01 08:27:28 +0000

    Nameless Bringup
    Signed-off-by: Manikantaraavi <raavimanikanta6595@gmail.com>

--- common.mk
+++ common.mk
@@ -34,7 +34,7 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-proton
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
