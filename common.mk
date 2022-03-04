41a0e1fec1fde78bba768e17e4c85509d7c5abbc
Keertesh <keertesh.k98@gmail.com>
2022-03-05 03:44:24 +0530

    sm7250-common: Build libqdMetaData.system
    *needed by libwfdmmsrc_system.so
    
    *F linker  : CANNOT LINK EXECUTABLE "/system_ext/bin/wfdservice": library "libqdMetaData.system.so" not found: needed by /system_ext/lib/libwfdmmsrc_system.so in namespace (default)
    
    Signed-off-by: manikantaraavi <raavimanikanta6595@gmail.com>

--- common.mk
+++ common.mk
@@ -246,6 +246,7 @@ PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
     libdisplayconfig.qti.vendor \
     libqdMetaData \
+    libqdMetaData.system \
     libsdmcore \
     libsdmutils \
     libtinyxml \
